2e2072a62b1b76d8050493497679edda1893acd2
Juan Antonio <jamll.muro@gmail.com>
2024-10-09 12:50:59 +0200

    Change the TopicPage to generate the metadata

--- src/app/topic/photos/[slug]/page.jsx
+++ src/app/topic/photos/[slug]/page.jsx
@@ -1,5 +1,18 @@
 import ListOfPhotos from "@/components/ListOfPhotos";
-import { getTopicImages } from "@/services/getDataFromAPI";
+import { getTopicImages, getTopics } from "@/services/getDataFromAPI";
+
+export async function generateMetadata({ params }) {
+  const slug = params.slug
+
+  const topics = await getTopics();
+
+  const topic = topics.filter((item) => item.slug === slug); 
+ 
+  return {
+    title: `Product Gallery | ${topic[0].title}`,
+    description: `${topic[0].description}`
+  }
+}
 
 export default async function TopicPage({ params }) {
 
