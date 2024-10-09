934d0e6fe2679a1f223c03d1c2bf1b743e715fa8
Juan Antonio <jamll.muro@gmail.com>
2024-10-09 14:29:33 +0200

    Add a new function that returns all the information of a specific image

--- src/services/getDataFromAPI.js
+++ src/services/getDataFromAPI.js
@@ -46,6 +46,22 @@ export async function getTopicImages({ slug }) {
   }
 }
 
+export async function getImage({ id }) {
+  try {    
+    const res = await fetch(`${process.env.API_URL}/photos/${id}`, {
+      headers: {
+        Authorization: `Client-ID ${process.env.ACCESS_KEY}`
+      }
+    })
+  
+    const data = await res.json();
+    
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
 export async function getSearchImages({ query }) {
   try {    
     const res = await fetch(`${process.env.API_URL}/search/photos?query=${query}&per_page=25`, {
