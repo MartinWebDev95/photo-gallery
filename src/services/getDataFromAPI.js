3c59ce444197528a07b29936f670fbe17ce4d619
Juan Antonio <jamll.muro@gmail.com>
2024-10-09 09:54:08 +0200

    Add a new function that return a list with random photos

--- src/services/getDataFromAPI.js
+++ src/services/getDataFromAPI.js
@@ -13,3 +13,19 @@ export async function getTopics() {
     throw new Error(error.message);
   }
 }
+
+export async function getRandomImages() {
+  try {    
+    const res = await fetch(`${process.env.API_URL}/photos/random?count=25`, {
+      headers: {
+        Authorization: `Client-ID ${process.env.ACCESS_KEY}`
+      }
+    })
+  
+    const data = await res.json();
+    
+    return data;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
