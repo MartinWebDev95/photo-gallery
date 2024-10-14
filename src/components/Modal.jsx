7aa417e65179167395bedf7fe811fc46074081a7
Juan Antonio <jamll.muro@gmail.com>
2024-10-14 14:34:35 +0200

    Change the Modal component to show the PhotoDetail component in a correct way

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,29 @@
 'use client'
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Modal = ({ children }) => {
   
   const router = useRouter();
+
+  const handleClick = (e) => {
+    if(e.target.getAttribute('data-name') !== null){
+      router.back();
+      document.body.style.overflow = 'auto';
+    }
+  }
+  
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+  }, []);
   
   return (
-    <dialog className='absolute top-0 left-0 bg-black/70 w-full flex justify-center' onClick={() => router.back()}>
-      <div className="mt-8 bg-[#bdbdbd] rounded-3xl w-4/5 flex justify-center overflow-y-auto">
-        {children}
+    <dialog data-name='dialog' className='absolute top-0 left-0 bg-black/70 w-full h-full flex justify-center cursor-pointer overflow-y-scroll' onClick={handleClick}>
+      <div className="my-8 bg-[#bdbdbd] rounded-3xl w-4/5 cursor-default h-fit">
+        <div className="my-12 px-12">
+          {children}
+        </div>
       </div>
     </dialog>
   )
