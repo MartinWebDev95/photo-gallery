0cdb9015d0afcd93518cb492a36db50b5183f731
Juan Antonio <jamll.muro@gmail.com>
2024-10-18 09:36:04 +0200

    Change some styles to make it responsive

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -15,13 +15,15 @@ const Modal = ({ children }) => {
   }
   
   useEffect(() => {
-    document.body.style.overflow = 'hidden';
+    if(window.innerWidth >= 1024){
+      document.body.style.overflow = 'hidden';
+    }
   }, []);
   
   return (
-    <dialog data-name='dialog' className='absolute top-0 left-0 bg-black/70 w-full h-full flex justify-center cursor-pointer overflow-y-scroll' onClick={handleClick}>
-      <div className="my-8 bg-[#bdbdbd] rounded-3xl w-4/5 cursor-default h-fit">
-        <div className="my-12 px-12">
+    <dialog data-name='dialog' className='absolute top-[365px] lg:top-0 left-0 bg-black/70 w-full h-full flex justify-center lg:cursor-pointer lg:overflow-y-scroll' onClick={handleClick}>
+      <div className="lg:my-8 bg-[#bdbdbd] lg:rounded-3xl w-full lg:w-4/5 cursor-default h-fit">
+        <div className="my-6 lg:my-12 px-4 lg:px-12">
           {children}
         </div>
       </div>
