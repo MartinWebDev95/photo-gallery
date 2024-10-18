314ea1bc7ac47b9975173b2408b3168536a60e35
Juan Antonio <jamll.muro@gmail.com>
2024-10-18 09:37:06 +0200

    Remove the Image component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,15 +1,14 @@
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
     <Link href="/" className='flex items-center gap-2 neomorphism_btn p-2'>
-      <Image 
+      <img 
         src="/assets/logo.png"
-        width={45}
-        height={45}
-        alt="Photo Gallery Logo" />
+        alt="Photo Gallery Logo"
+        className='w-10 h-10' 
+        loading='lazy' />
 
       <p>Photo Gallery</p>
     </Link>
