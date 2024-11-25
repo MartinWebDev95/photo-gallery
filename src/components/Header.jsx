764e5058e64a6a717242132dca621eda2f6031fa
Juan Antonio <jamll.muro@gmail.com>
2024-11-25 12:18:39 +0100

    Change the web main color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Logo from "./Logo"
 
 const Header = () => {
   return (
-    <header className="pt-4 bg-amber-200">
+    <header className="pt-4 bg-emerald-200">
       <div className="container mx-auto px-4 lg:px-0">
         <div className="flex items-center flex-col gap-8 lg:flex-row lg:justify-between">
           <Logo />
